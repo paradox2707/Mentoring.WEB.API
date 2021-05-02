bfef5eb6e7ad9d6cfd0b5ea65edb2867aa967c6d
paradox <sluva2707@gmail.com>
2021-05-02 15:07:59 +0300

    wip

--- Mentoring.Client/Mentoring.Client/Helpers/EnumHelper.cs
+++ Mentoring.Client/Mentoring.Client/Helpers/EnumHelper.cs
@@ -1,30 +1,27 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
 
 namespace Mentoring.Client.Helpers
 {
     static class EnumHelper
     {
-        public static string GetDescription(this Enum value)
+        public static string GetDescription(this Enum enumeration)
         {
-            Type type = value.GetType();
-            string name = Enum.GetName(type, value);
-            if (name != null)
-            {
-                FieldInfo field = type.GetField(name);
-                if (field != null)
-                {
-                    DescriptionAttribute attr =
-                           Attribute.GetCustomAttribute(field,
-                             typeof(DescriptionAttribute)) as DescriptionAttribute;
-                    if (attr != null)
-                    {
-                        return attr.Description;
-                    }
-                }
-            }
-            return null;
+            DescriptionAttribute attribute = 
+                enumeration
+                .GetType()
+                .GetMember(enumeration.ToString())
+                .FirstOrDefault(member => member.MemberType == MemberTypes.Field)
+                .GetCustomAttributes(typeof(DescriptionAttribute), false)
+                .Cast<DescriptionAttribute>()
+                .SingleOrDefault();
+
+            if (attribute == null)
+                throw new Exception("Doesn`t have description attribute");
+
+            return attribute.Description;
         }
     }
 }
