f8816528ccd497362423efebbd5baf0a396e8718
paradox <sluva2707@gmail.com>
2021-06-03 02:04:30 +0300

    wip web api

--- Mentoring.WEB.API.BLL/Implementations/Services/UniversityService.cs
+++ Mentoring.WEB.API.BLL/Implementations/Services/UniversityService.cs
@@ -8,6 +8,8 @@ using Mentoring.WEB.API.DAL.Interfaces;
 using System.Linq;
 using System;
 using Mentoring.WEB.API.Common.FilterModels;
+using System.Linq.Expressions;
+using System.Reflection;
 
 namespace Mentoring.WEB.API.BLL.Implementations.Services
 {
@@ -33,28 +35,72 @@ namespace Mentoring.WEB.API.BLL.Implementations.Services
 
         public async Task<IEnumerable<UniversityModel>> GetAllByFilterAsync(UniversityFilter filter)
         {
-            var daos = await _universityRepo.GetAllAsync();
-            if (string.IsNullOrWhiteSpace(filter.Region))
-                daos = daos.Where(e => e.Region.Name == filter.Region).ToList();
-            if (filter.IsGoverment != null)
-                daos = daos.Where(e => e.IsGoverment == filter.IsGoverment).ToList();
-            if (string.IsNullOrWhiteSpace(filter.SearchText))
-                daos = GetUniversityByFilter(filter.SearchText.TrimEnd().ToLower(), daos);
+            var filterExpression = CreateFilterExpressionForUniversity(filter);
+            var daos = await _universityRepo.GetAllByAsync(filterExpression);
             return _mapper.Map<List<University>, IEnumerable<UniversityModel>>(daos);
         }
-
-        private static List<University> GetUniversityByFilter(string filter, List<University> daos) => 
-            daos.Where(e => e.Name.ToLower().Split(new string[] { " ", "  ", "   ", "\t" }, StringSplitOptions.RemoveEmptyEntries).Any(word => word.StartsWith(filter))
-            || e.Name.ToLower().StartsWith(filter)
-            || e.ShortName.ToLower().Split(new string[] { " ", "  ", "   ", "\t" }, StringSplitOptions.RemoveEmptyEntries).Any(word => word.StartsWith(filter))
-            || e.ShortName.ToLower().StartsWith(filter))
-            .ToList();
 
         public async Task<IEnumerable<UniversityModel>> GetAllWithSpecialitiesAsync()
         {
             var daos = await _universityRepo.GetAllWithSpecialiesAsync();
 
             return _mapper.Map<List<University>, IEnumerable<UniversityModel>>(daos);
+        }
+
+        private Expression<Func<University, bool>> CreateFilterExpressionForUniversity(UniversityFilter filter)
+        {
+            // univer =>
+            ParameterExpression inputEntity = Expression.Parameter(typeof(University), "univer");
+            Expression predicateBody = Expression.Empty();
+            var conditionsList = new List<Expression>();
+
+            // univer.ToLower().Contains(filter.SearchText.ToLower())
+            if (!string.IsNullOrWhiteSpace(filter.SearchText))
+            {
+                Expression leftProp = Expression.Property(inputEntity, typeof(University).GetProperty("Name"));
+                Expression leftCall = Expression.Call(leftProp, typeof(string).GetMethod("ToLower", System.Type.EmptyTypes));
+                Expression right = Expression.Constant(filter.SearchText.ToLower());
+
+                MethodInfo method = typeof(string).GetMethod("Contains", new[] { typeof(string) });
+
+                var exp = Expression.Call(leftCall, method, right);
+                conditionsList.Add(exp);
+            }
+
+            // univer.Region.Name.ToLower() == filter.Region.ToLower()
+            if (!string.IsNullOrWhiteSpace(filter.Region) && filter.Region != "Вся Україна")
+            {
+                Expression leftProp = Expression.Property(inputEntity, typeof(University).GetProperty("Region"));
+                Expression leftPropValue = Expression.Property(leftProp, typeof(Region).GetProperty("Name"));
+                Expression leftCall = Expression.Call(leftPropValue, typeof(string).GetMethod("ToLower", System.Type.EmptyTypes));
+                Expression right = Expression.Constant(filter.Region.ToLower());
+                Expression exp = Expression.Equal(leftCall, right);
+                conditionsList.Add(exp);
+            }
+
+            // univer.IsGoverment == true
+            if (filter.IsGoverment.GetValueOrDefault())
+            {
+                Expression leftProp = Expression.Property(inputEntity, typeof(University).GetProperty("IsGoverment"));
+                Expression right = Expression.Constant(true);
+                Expression exp = Expression.Equal(leftProp, right);
+                conditionsList.Add(exp);
+            }
+
+            if (!conditionsList.Any())
+                predicateBody = Expression.Constant(true);
+
+            foreach (var conditionItem in conditionsList)
+            {
+                if (conditionsList.First() == conditionItem)
+                {
+                    predicateBody = conditionItem;
+                    continue;
+                }
+                predicateBody = Expression.AndAlso(predicateBody, conditionItem);
+            }
+
+            return Expression.Lambda<Func<University, bool>>(predicateBody, new ParameterExpression[] { inputEntity });
         }
     }
 }
--- Mentoring.WEB.API.DAL/Interfaces/IUniversityRepository.cs
+++ Mentoring.WEB.API.DAL/Interfaces/IUniversityRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Mentoring.WEB.API.DAL.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Mentoring.WEB.API.DAL.Interfaces
@@ -8,5 +10,6 @@ namespace Mentoring.WEB.API.DAL.Interfaces
     {
         Task<List<University>> GetAllAsync();
         Task<List<University>> GetAllWithSpecialiesAsync();
+        Task<List<University>> GetAllByAsync(Expression<Func<University, bool>> expression);
     }
 }
